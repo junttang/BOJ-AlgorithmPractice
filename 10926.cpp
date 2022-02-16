39262047d1e79607511f035c73e1fed4f65b2407
Hyeok's Git <81617508+junttang@users.noreply.github.com>
2022-02-16 20:05:00 +0900

    Add files via upload

---
+++ 10926.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+// BOJ - 10926 ??!
+
+using namespace std;
+
+int main(void) {
+	string already;
+
+	cin >> already;
+	cout << already << "??!";
+
+	return 0;
+}
