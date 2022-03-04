da1ed194a5d61bf3ea84eebe56605b7f192cbf13
Hyeok's Git <81617508+junttang@users.noreply.github.com>
2022-03-04 11:13:33 +0900

    Add files via upload

---
+++ 11399.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+// BOJ - 11399 ATM
+using namespace std;
+
+int p[1000];
+
+int solve(int n) {
+	int sum = 0;
+	sort(p, p + n);
+
+	for (int i = 0; i < n; i++)
+		for (int j = n - i - 1; j >= 0; j--) 
+			sum += p[i];
+
+	return sum;
+}
+
+int main(void) {
+	int n;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> p[i];
+	cout << solve(n);
+
+	return 0;
+}
