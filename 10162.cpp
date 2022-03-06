2edfd2c0e9f744f661b5df5fc5244334f81e3d2b
Hyeok's Git <81617508+junttang@users.noreply.github.com>
2022-03-06 15:54:13 +0900

    Add files via upload

---
+++ 10162.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+// BOJ - 10162 Microwave
+using namespace std;
+
+void solve(int time) { 
+	int a, b, c;
+	if (time % 10 != 0) { cout << -1; return; }
+
+	a = time / 300;
+	time %= 300;
+	b = time / 60;
+	time %= 60;
+	c = time / 10;
+
+	cout << a << ' ' << b << ' ' << c;
+}
+
+int main(void) {
+	int t;
+
+	cin >> t;
+	solve(t);
+
+	return 0;
+}
