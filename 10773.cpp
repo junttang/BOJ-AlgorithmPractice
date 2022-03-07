8799860e4803411772cb38d332f962b04d1bd352
Hyeok's Git <81617508+junttang@users.noreply.github.com>
2022-03-07 18:52:38 +0900

    Add files via upload

---
+++ 10773.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <stack>
+// BOJ - 10773 Zero
+using namespace std;
+
+stack<int> s;
+
+int main(void) {
+	int k, num, sum = 0;
+
+	cin >> k;
+	while (k--) {
+		cin >> num;
+		if (num == 0) s.pop();
+		else s.push(num);
+	}
+
+	while (!s.empty()) {
+		sum += s.top();
+		s.pop();
+	}
+	cout << sum;
+
+	return 0;
+}
