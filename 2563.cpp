fcefb4543298ac9501397f4b1af1568aa1fea441
Hyeok's Git <81617508+junttang@users.noreply.github.com>
2022-03-11 16:24:37 +0900

    Add files via upload

---
+++ 2563.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+// BOJ - 2563 Colorpaper
+using namespace std;
+
+bool paper[101][101];
+
+int solve(void) {
+	int cnt = 0;
+
+	for (int i = 1; i <= 100; i++) 
+		for (int j = 1; j <= 100; j++) 
+			if (paper[i][j]) cnt++;
+
+	return cnt;
+}
+
+int main(void) {
+	int n, x, y;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		for (int j = 0; j < 10; j++) 
+			for (int k = 0; k < 10; k++) 
+				paper[x + j][y + k] = true;
+	}
+	cout << solve();
+
+	return 0;
+}
