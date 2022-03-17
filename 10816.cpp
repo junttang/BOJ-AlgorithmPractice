b6873f9261907135b94c4ff12ac0a532fedfa57a
Hyeok's Git <81617508+junttang@users.noreply.github.com>
2022-03-18 01:15:30 +0900

    Add files via upload

---
+++ 10816.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+// BOJ - 10816 Number Card 2
+#define MAX_SIZE	500000
+#define HASH_SIZE	10000000
+using namespace std;
+
+int target[MAX_SIZE + 1], cnt[2*HASH_SIZE + 1];
+
+int main(void) {
+	int n, m, temp;
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) { cin >> temp; cnt[temp + HASH_SIZE]++; }
+	cin >> m;
+	for (int i = 1; i <= m; i++) cin >> target[i];
+
+	for (int i = 1; i <= m; i++) 
+		cout << cnt[target[i] + HASH_SIZE] << '\n';
+
+	return 0;
+}
