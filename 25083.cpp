b514b789cb6e3cc14dbd75c8627fccb710f27dcc
Hyeok's Git <81617508+junttang@users.noreply.github.com>
2022-08-03 16:13:39 +0900

    Create 25083.cpp

---
+++ 25083.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// BOJ 25083 - New Seeds
+#define print(A) cout<<A<<endl;
+using namespace std;
+
+int main(void) {
+	print("         ,r'\"7");
+	print("r`-_   ,'  ,/");
+	print(" \\. \". L_r'");
+	print("   `~\\/");
+	print("      |");
+	print("      |");
+
+	return 0;
+}
